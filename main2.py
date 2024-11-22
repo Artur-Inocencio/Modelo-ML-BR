e6cb6bb714d9ef5262e53fc036dbc264218c196e
Artur Inocêncio ferreira dos santos <90269415+Artur-Inocencio@users.noreply.github.com>
2024-11-21 22:19:18 -0300

    first commit

---
+++ main2.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+# Configuração da API Flask
+app = Flask(__name__)
+CORS(app)
+
+# Função para normalizar os dados de entrada
+def preprocess_input(data):
+    # Ajustar para incluir as colunas corretas no DataFrame de entrada
+    input_data = pd.DataFrame({
+        'Pessoas': [data['Pessoas']],
+        'Veículos': [data['Veículos']],
+        #'Data e Hora': [pd.Timestamp(data['Data e Hora'])],
+        'Sentido': [data['Sentido']],
+        'Clima': [data['Clima']],
+        'Pista': [data['Pista']],
+        'Traçado': [data['Traçado']],
+        'UF': [data['UF']],
+        'BR': [data['BR']]
+    })
+    return input_data
+
+# Carregar o modelo treinado
+with open('C:\\Users\\artur\\Downloads\\PROJETO\\modelo-prf-br.pkl', 'rb') as model_file:
+    modelo_completo = pickle.load(model_file)
+
+@app.route('/prever', methods=['POST'])
+def prever_probabilidade():
+    try:
+        # Receber dados da solicitação em formato JSON
+        dados = request.get_json()
+        #print("Dados:", dados)  
+        # Pré-processar os dados
+        input_data = preprocess_input(dados)
+        if modelo_completo is None: 
+            print("Dados:", input_data) 
+        # Fazer a previsão
+        probabilidade_ileso = modelo_completo.predict_proba(input_data)[:, 1]
+        
+        # Construir a resposta
+        resultado = {
+            'probabilidade_ileso': float(probabilidade_ileso[0])
+        }
+
+        # Retornar a resposta como JSON
+        return jsonify(resultado)
+    except Exception as e:
+        # Tratar erros e retornar mensagem de erro
+        return jsonify({'erro': str(e)}), 400
+
+# Executar o servidor Flask
+if __name__ == '__main__':
+    app.run(debug=True)
